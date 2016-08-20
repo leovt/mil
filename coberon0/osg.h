09dd8b3ad677be6a65efbc42be9a0d8d91f0c484
leovt <leonhard.vogt@gmx.ch>
2016-08-20 22:37:04 +0200

    parse factor

--- coberon0/osg.h
+++ coberon0/osg.h
@@ -16,6 +16,13 @@ typedef enum _class{
   Jmp
 } Class;
 
+typedef enum _form{
+  Boolean,
+  Integer,
+  Array,
+  Record
+} Form;
+
 typedef struct _ObjDesc *Object;
 typedef struct _TypeDesc *Type;
 
@@ -30,5 +37,32 @@ typedef struct _ObjDesc {
   Object dsc;
   int val;
 } ObjDesc;
+
+typedef struct _Item {
+  Class mode;
+  int lev;
+  Type type;
+  int a,b,r,idx;
+} Item;
+
+typedef struct _TypeDesc {
+  Form form;
+  Object fields;
+  Type base;
+  int size;
+  int len;
+} TypeDesc;
+
+extern Type intType;
+extern Type boolType;
+
+extern Item MakeItem(Object);
+extern Item MakeConstItem(Type, int);
+
+extern void Index(Item, Item);
+extern void Field(Item, Object);
+extern void Op1(symbol, Item);
+extern void Op2(symbol, Item, Item);
+
 
 #endif
--- coberon0/osp.c
+++ coberon0/osp.c
@@ -9,6 +9,7 @@ static Object topScope;
 static Object universe;
 static Object guard;
 
+static symbol sym;
 
 Object NewObject(Class class){
   Object x = topScope;
@@ -38,11 +39,129 @@ static Object find(void){
     while(strncmp(x->name, id, ID_LENGTH)){
       x = x->next;
     }
-    if (x != guard) return x;
+    if (x != guard) {
+      return x;
+    }
     if (s == universe) {
       Mark("undef");
       return x;
     }
     s = s->dsc;
+  }
+}
+
+
+static Object FindField(Object list){
+  strncpy(guard->name, id, ID_LENGTH);
+  while(strncmp(list->name, id, ID_LENGTH)){
+    list = list->next;
+  }
+  return list;
+}
+
+
+static bool IsParam(Object obj){
+  return (obj->class == Par) || (obj->class == Var && obj->val > 0);
+}
+
+
+static void OpenScope(void){
+  Object s = malloc(sizeof(ObjDesc));
+  s->class = Head;
+  s->dsc = topScope;
+  s->next = guard;
+  topScope = s;
+}
+
+
+static void CloseScope(void){
+  topScope = topScope->dsc;
+}
+
+/* ----------- Parser ------------ */
+
+Item expression(void);
+
+void selector(const Item x){
+  while(sym == lbrak || sym == period) {
+    if (sym == lbrak) {
+      sym = Get();
+      Item y = expression();
+
+      if (x.type->form == Array){
+	Index(x, y);
+      }
+      else { 
+	Mark("not an array"); 
+      }
+      
+      if (sym == rbrak) {
+	sym = Get();
+      }
+      else{
+	Mark("]?");
+      }
+    }
+    else{
+      sym = Get();
+      if (sym == ident) {
+	if (x.type->form == Record){
+	  Object obj = FindField(x.type->fields);
+	  sym = Get();
+	  if (obj != guard){
+	    Field(x, obj);
+	  }
+	  else{
+	    Mark("undef");
+	  }
+	}
+	else{
+	  Mark("not a record");
+	}
+      }
+    }
+  }
+}
+
+
+Item factor(void){
+  if (sym < lparen){
+    Mark("ident?");
+    while (sym < lparen) sym = Get();
+  }
+
+  switch(sym){
+  case ident: {
+    Object obj = find();
+    sym = Get();
+    Item x = MakeItem(obj);
+    selector(x);
+    return x;
+  }
+  case number: {
+    int value = val;
+    sym = Get();
+    return MakeConstItem(intType, value);
+  }
+  case lparen: {
+    sym = Get();
+    Item x = expression();
+    if (sym == rparen) {
+      sym = Get();
+    }
+    else {
+      Mark(")?");
+    }
+    return x;
+  }
+  case not: {
+    sym = Get();
+    Item x = factor();
+    Op1(not, x);
+    return x;
+  }
+  default:
+    Mark("factor?");
+    return MakeItem(guard);
   }
 }
