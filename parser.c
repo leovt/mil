624098504694bd1c02951b0e0647933b3a75ee91
leovt <leonhard.vogt@gmx.ch>
2016-09-01 16:55:30 +0200

    parsing statements and blocks

--- parser.c
+++ parser.c
@@ -11,11 +11,170 @@ struct Node{
   Node *sibling;
 };
 
+static const Token LIST={TK_LIST,0,0};
+static const Token ERROR={TK_ERROR,0,0};
+
 typedef Node *AST;
 
-AST parse(LexerState* ls){
-  (void) ls;
-  return 0;
+typedef struct ParserState{
+  LexerState* ls;
+  Token current;
+} ParserState;
+
+static AST make_node(Token tok){
+  AST node = malloc(sizeof(Node));
+  node->tok = tok;
+  node->sibling = 0;
+  node->child = 0;
+  return node;
+}
+
+Token get_filtered(LexerState* ls){
+  for(;;){
+    Token tok = get_token(ls);
+    if(tok.type != TK_COMMENT){
+      return tok;
+    }
+  }
+}
+
+Token consume(ParserState* ps, TokenType expect, const char* msg){
+  if (ps->current.type == expect) {
+    Token tok = ps->current;
+    ps->current = get_filtered(ps->ls);
+    return tok;
+  }
+  else{
+    fprintf(stderr, "%s: expected %s but got %s.\n",
+	    msg, TokenNames[expect], TokenNames[ps->current.type]);
+    return ERROR;
+  }
+}
+
+ParserState* initialize(const char* src){
+  ParserState* ps = malloc(sizeof(ParserState));
+  ps->ls = lexer_initialize(src);
+  ps->current = get_filtered(ps->ls);
+  return ps;
+}
+
+AST block(ParserState*);
+
+AST function_definition(ParserState* ps){
+  Token def = consume(ps, TK_DEF, "function definition");
+  Token name = consume(ps, TK_IDENTIFIER, "function definition");
+  consume(ps, TK_LPAREN, "function definition");
+  Token arg = consume(ps, TK_IDENTIFIER, "function definition");
+  consume(ps, TK_COLON, "function definition");
+  Token argtype = consume(ps, TK_IDENTIFIER, "function definition");
+  consume(ps, TK_RPAREN, "function definition");
+  consume(ps, TK_COLON, "function definition");
+  consume(ps, TK_NEWLINE, "function definition");
+  consume(ps, TK_INDENT, "function body");
+  AST body = block(ps);
+  consume(ps, TK_DEDENT, "function body");
+
+  AST function = make_node(def);
+  function->child = make_node(name);
+  function->child->sibling = body;
+  body->sibling = make_node(arg);
+  body->sibling->child = make_node(argtype);
+
+  return function;
+}
+
+AST conditional(ParserState* ps){
+  Token tok = consume(ps, TK_IF, "if statement");
+  AST cond=make_node(ERROR);
+  while (ps->current.type != TK_COLON)
+    consume(ps, ps->current.type, 0);
+  consume(ps, TK_COLON, "if statement");
+  consume(ps, TK_NEWLINE, "if statement");
+  consume(ps, TK_INDENT, "if statement");
+  AST true_block = block(ps);
+  consume(ps, TK_DEDENT, "if statement");
+
+  AST conditional = make_node(tok);
+  conditional->child = cond;
+  cond->sibling = true_block;
+
+  return conditional;
+}
+
+
+AST loop_stmt(ParserState* ps){
+  Token tok = consume(ps, TK_LOOP, "loop statement");
+  consume(ps, TK_COLON, "loop statement");
+  consume(ps, TK_NEWLINE, "loop statement");
+  consume(ps, TK_INDENT, "loop statement");
+  AST body = block(ps);
+  consume(ps, TK_DEDENT, "loop statement");
+
+  AST loop = make_node(tok);
+  loop->child = body;
+
+  return loop;
+}
+
+AST return_stmt(ParserState* ps){
+  Token ret = consume(ps, TK_RETURN, "return statement");
+  AST expr = make_node(ERROR);
+  while (ps->current.type != TK_NEWLINE)
+    consume(ps, ps->current.type, 0);
+  consume(ps, TK_NEWLINE, "return statement");
+
+  AST return_stmt = make_node(ret);
+  return_stmt->child = expr;
+  return return_stmt;
+}
+
+AST expression_stmt(ParserState* ps){
+  AST expr = make_node(ERROR);
+  while (ps->current.type != TK_NEWLINE)
+    consume(ps, ps->current.type, 0);
+  consume(ps, TK_NEWLINE, "expression or assignment statement");
+
+  return expr;
+}
+
+AST statement(ParserState* ps){
+  switch(ps->current.type){
+  case TK_DEF:
+    return function_definition(ps);
+  case TK_IF:
+    return conditional(ps);
+  case TK_RETURN:
+    return return_stmt(ps);
+  case TK_LOOP:
+    return loop_stmt(ps);
+  case TK_BREAK:
+    {
+      Token tok = ps->current;
+      consume(ps, TK_BREAK, "break statement");
+      consume(ps, TK_NEWLINE, "break statement");
+      return make_node(tok);
+    }
+  default:
+    return expression_stmt(ps);
+  }
+}
+
+AST block(ParserState* ps){
+  AST head = make_node(LIST);
+  AST current_stmt = statement(ps);
+  head->child = current_stmt;
+  
+  while(ps->current.type != TK_DEDENT && ps->current.type != TK_EOF) {
+    AST stmt = statement(ps);
+    current_stmt->sibling = stmt;
+    current_stmt = stmt;
+  }
+
+  return head;
+}
+
+AST parse(ParserState* ps){
+  return block(ps);
 }
 
 void dump_ast(AST ast, int level){
@@ -67,35 +226,17 @@ return buffer;
 }
 
 int main(int argc, char** argv){
-
-Node c = {{TK_IDENTIFIER, 0, "c"},0,0};
-Node b = {{TK_IDENTIFIER, 0, "b"},0,&c};
-Node bc = {{TK_TIMES, 0, 0},&b,0};
-Node a = {{TK_IDENTIFIER, 0, "a"},0,&bc};
-Node a_bc = {{TK_PLUS,0,0},&a,0};
-
-dump(&a_bc);
-
-
-
-
   if(argc != 2){
     if(argc){
       printf("Usage: %s filename\n", argv[0]);
     }else{
       printf("Usage: need one filename argument\n");
     }
-  }else{
-   
+  }
+  else {
     char* src = read(argv[1]);
-    
-    LexerState *ls = lexer_initialize(src);
-
-    AST ast = parse(ls);
-
-    lexer_close(ls);
-
+    ParserState *ps = initialize(src);
+    AST ast = parse(ps);
     dump(ast);
-
   }
 }
